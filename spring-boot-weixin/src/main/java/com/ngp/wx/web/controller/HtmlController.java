7f2f6669af30a4bba3a01fa0c92f22960c6b7a63
zhangyk <zhangyuankai@spdbdev.com>
2018-08-15 19:57:25 +0800

    框架优化

--- spring-boot-weixin/src/main/java/com/ngp/wx/web/controller/HtmlController.java
+++ spring-boot-weixin/src/main/java/com/ngp/wx/web/controller/HtmlController.java
@@ -1,8 +1,14 @@
 package com.ngp.wx.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/wxh")
 public class HtmlController {
 
+    @RequestMapping("/login")
+    public String loginHtml(){
+        return "test";
+    }
 }
